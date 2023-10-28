872d273c542e6a6a783926a06e9ded656e012b91
Najafizadeah <A.latifnajafizadeh@gmil.com>
2023-10-28 17:03:21 +0330

    complete def fetch medal

--- ODA-WebApp/app.py
+++ ODA-WebApp/app.py
@@ -1,7 +1,22 @@
 import streamlit as st
 import pandas as pd
+import preprocesseor,helper
 
-st.sidebar.radio(
+
+df = pd.read_csv('/home/abdollatif/Desktop/ODA/athlete_events.csv')
+region_df = pd.read_csv('/home/abdollatif/Desktop/ODA/noc_regions.csv')
+
+df = preprocesseor.preprocess(df, region_df)
+st.sidebar.title("Olympic Analysis")
+user_menu = st.sidebar.radio(
                  'select an option',
                  ('Medal Tally','Overall Analysis','Country-wise Analysis','Atlete wise Analysis')
 )
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country = helper.country_year_list(df)
+    select_year = st.sidebar.selectbox("select year",years)
+    select_year = st.sidebar.selectbox("select country",country)
+    medal_tally = helper.medal_tally(df)
+    st.dataframe(medal_tally)
